e30200b658af3b5298ea851e2a3cd44da739e7ae
ayaya <ayaya6436@gmail.com>
2023-06-25 22:54:34 +0000

    page acceuil

--- index.php
+++ index.php
@@ -3,12 +3,57 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Portail ODC</title>
 </head>
 <body>
     <div class="container">
-        <div class="navbar"></div>
+        <div class="navbar">
+            <div class="title"><span>ORANGE DIGITAL CENTER</span></div>
+            <div class="menu">
+                <ul>
+                    <li><a href="#">Formations</a></li>
+                    <li class="separateur">|</li>
+                    <li><a href="#">Entreprises</a></li>
+                </ul>
+            </div>
+
+            <div class="connect">
+                <!-- <div class="search">
+                    <input type="search">
+                </div> -->
+                <div class="btn-connect">
+                    <button class="btn1">Connexion</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="content">
+           <div class="content-text">
+           <div class="content1">
+                <h1>Des competences <br>
+                    d'aujourd'hui qui ont de <br>
+                    l'avenir
+
+                </h1>
+            </div>
+            <div class="content2">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatume<br>
+                 explicabo natus fugit quae impedit rerum aut debitis dolordsh <br>
+                Natus amet illum temporibus autem aperiam!
+            </div>
+
+            <div class="content3">
+                <button class="btn2">Inscription</button>
+                <button class="btn3">Formations</button>
+            </div>
+
+           </div>
+
+            <div class="content-image">
+                <img src="assets/images/undraw_book_lover_re_rwjy.svg" alt="image1">
+            </div>
+        </div>
     </div>
 </body>
 </html>
