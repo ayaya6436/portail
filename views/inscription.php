0bff43ea5d46231cf76039bc34b365a7856a8a80
ayaya <ayaya6436@gmail.com>
2023-06-26 09:15:35 +0000

    debut page inscription

---
+++ views/inscription.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://localhost/portail/assets/css/inscription.css">
+    <title>Formulaire</title>
+</head>
+<body>
+    <div class="container">
+       
+        <div class="content2">
+            <h1>Creer un compte</h1>
+            <div class="formulaire">
+                <form action="">
+                    <div class="row">
+                        <div class="row1">
+                            <div>
+                                <label for="">Matricule</label>
+                                <input type="text">
+                            </div>
+                            <div>
+                                <label for="">Matricule</label>
+                                <input type="text">
+                            </div>
+                        </div>
+
+                        
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
