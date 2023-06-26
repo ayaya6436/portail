edb5bc26bb313d6e7db32514ed438ef316da955d
ayaya <ayaya6436@gmail.com>
2023-06-26 10:44:01 +0000

    fin front page inscription

--- views/inscription.php
+++ views/inscription.php
@@ -8,28 +8,83 @@
 </head>
 <body>
     <div class="container">
-       
-        <div class="content2">
-            <h1>Creer un compte</h1>
-            <div class="formulaire">
-                <form action="">
-                    <div class="row">
-                        <div class="row1">
-                            <div>
-                                <label for="">Matricule</label>
-                                <input type="text">
-                            </div>
-                            <div>
-                                <label for="">Matricule</label>
-                                <input type="text">
-                            </div>
-                        </div>
 
-                        
+                <div class="content1">
+                    <div class="title"><span>ORANGE DIGITAL CENTER</span></div>
+
+                </div>
+                
+            <form action="" class="formulaire">
+        
+                
+
+                <div class="row2">
+                    <div class="row-colunm">
+                        <label for="">Email</label>
+                        <input type="email" name="email">
                     </div>
-                </form>
-            </div>
-        </div>
+
+                    <div class="row-colunm">
+                        <label for="">Telephone</label>
+                        <input type="text" name="telephone">
+                    </div>
+
+                    <div class="row-colunm">
+                        <label for="">Photos</label>
+                        <input type="file" name="photo">
+                    </div>
+
+                    <div class="row-colunm">
+                        <label for="">Promotion</label>
+                        <select name="promotion" type="text">
+                            <option value="">ODK3</option>
+                        </select>
+                    </div>
+
+                    <div class="row-colunm">
+                        <label for="">Annee</label>
+                        <input type="date" name="annee">
+                    </div>
+                </div>
+
+                <div class="row1">
+                    <div class="row-colunm">
+                        <label for="">Matricule</label>
+                        <input type="text" name="matricule">
+                    </div>
+
+                    <div class="row-colunm">
+                        <label for="">Nom</label>
+                        <input type="text" name="nom">
+                    </div>
+
+                    <div class="row-colunm">
+                        <label for="">Prenom</label>
+                        <input type="text" name="prenom">
+                    </div>
+
+                    <div class="row-colunm">
+                        <label for="">Age</label>
+                        <input type="text" name="age">
+                    </div>
+
+                    <div class="row-colunm">
+                        <label for="">Date de naissance</label>
+                        <input type="date" name="dateNaissance">
+                    </div>
+                </div>
+
+                <div class="row3">
+                    <button class="btn">Enregistrer</button>
+                </div>
+                
+            </form>
+
+            
+            
+
+            
+      
     </div>
 </body>
 </html>
